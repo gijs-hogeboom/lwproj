bd163311c727176a9d37330b55bc70e38ef14021
Gijs Hogeboom <gijs.hogeboom@wur.nl>
2025-10-01 16:50:25 +0200

    steady progress

--- src/MC.cpp
+++ src/MC.cpp
@@ -3,29 +3,17 @@
 #include <vector>
 
 
-std::vector<float> run_MC(const std::vector<float>& arr_x,
-                          const std::vector<float>& arr_y,
-                          const std::vector<float>& arr_z,
-                          const std::vector<float>& arr_xh,
-                          const std::vector<float>& arr_yh,
+std::vector<float> run_MC(const std::vector<float>& arr_z,
                           const std::vector<float>& arr_zh,
                           const std::vector<float>& arr_dz,
-                          const std::vector<float>& field_kext,
-                          const std::vector<float>& arr_field_bounds,
-                          const std::vector<float>& arr_photons_atm_pos_x,
-                          const std::vector<float>& arr_photons_atm_pos_y,
-                          const std::vector<float>& arr_photons_atm_pos_z,
-                          const std::vector<float>& arr_photons_atm_mu,
-                          const std::vector<float>& arr_photons_atm_az,
-                          const std::vector<float>& arr_photons_atm_tau,
-                          const std::vector<float>& arr_power_per_photon_atm,
-                          const std::vector<float>& arr_photons_sfc_pos_x,
-                          const std::vector<float>& arr_photons_sfc_pos_y,
-                          const std::vector<float>& arr_photons_sfc_pos_z,
-                          const std::vector<float>& arr_photons_sfc_mu,
-                          const std::vector<float>& arr_photons_sfc_az,
-                          const std::vector<float>& arr_photons_sfc_tau,
-                          const std::vector<float>& arr_power_per_photon_sfc,
+                          const std::vector<float>& arr_kext,
+                          const std::vector<float>& arr_Batm,
+                          float Bsfc,
+                          float dx,
+                          float dy,
+                          int ktot,
+                          int jtot,
+                          int itot,
                           const std::string& CASE,
                           const std::string& INTERCELL_TECHNIQUE,
                           const std::string& INTRACELL_TECHNIQUE,
@@ -33,13 +21,78 @@ std::vector<float> run_MC(const std::vector<float>& arr_x,
                           int Nsfc)
 {
 
-    std::vector<float> arr_heating_rates;
-    arr_heating_rates.reserve(arr_z.size());
+    // Initializing MC fields
 
-    for (auto el : arr_z)
+    int n_volumes = itot * jtot * ktot;
+    int n_tiles   = jtot * ktot;
+
+    std::vector<float> arr_xh(ktot + 1);
+    std::vector<float> arr_yh(jtot + 1);
+    std::vector<float> arr_x(ktot);
+    std::vector<float> arr_y(jtot);
+
+
+    for (int j = 0; j < (jtot + 1); j++)
     {
-        arr_heating_rates.emplace_back(1.0 * el);
+        arr_yh[j] = j*dy;
+        if (j < jtot)
+        {
+            arr_y[j] = (j*dy + (j+1)*dy)/2;
+        }
     }
+    for (int k = 0; k < (ktot + 1); k++)
+    {
+        arr_xh[k] = k*dx;
+        if (k < ktot)
+        {
+            arr_x[k] = (k*dx + (k+1)*dx)/2;
+        }
+    }
+
+    // Initializing fields
+    std::vector<float> field_atm_kext(n_volumes);
+    std::vector<float> field_atm_B(n_volumes);
+    std::vector<float> field_atm_phi(n_volumes);
+    std::vector<float> field_sfc_B(n_tiles);
+    std::vector<float> field_sfc_phi(n_tiles);
+    std::vector<float> field_sfc_eps(n_tiles, 1.0f);
+
+    std::vector<float> field_atm_absorbed(n_volumes);
+    std::vector<float> field_atm_emitted(n_volumes);
+    std::vector<float> field_sfc_absorbed(n_tiles);
+    std::vector<float> field_sfc_emitted(n_tiles);
+
+    // Generating fields
+    for (int i = 0; i < itot; i++)
+    {
+        for (int j = 0; j < jtot; j++)
+        {
+            for (int k = 0; k < ktot; k++)
+            {
+                int idx_atm = i * ktot * jtot + j * ktot + k;
+                field_atm_kext[idx_atm] = arr_kext[i];
+                field_atm_B[idx_atm] = arr_Batm[i];
+                field_atm_phi[idx_atm] = 4*
+
+                if (i == 0)
+                {
+                    int idx_sfc = j * ktot + k;
+                    field_sfc_B[idx_sfc] = Bsfc;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    std::vector<float> arr_heating_rates(1, 0.0f);
 
     return arr_heating_rates;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,22 @@ int main()
 {
 
     // Control variables
-    std::string CASE = "gpt3";
+    std::string CASE = "gpt3";                  // {gpt0, gpt1, gpt3, gpt21}
+    bool ENABLE_MC = true;                      // Enables Monte Carlo algorithm
+    bool ENABLE_PP = false;                     // Enables plane-parallel algorithm
+    float dx = 1e8;                             // [m]
+    float dy = 1e8;                             // [m]
 
-    int Nphotpow = 21;
-    std::string INTERCELL_TECHNIQUE = "power";
-    std::string INTRACELL_TECHNIQUE = "naive";
+    int Nphotpow = 21;                          // Total 2^pow number of photons
+    std::string INTERCELL_TECHNIQUE = "power";  // {uniform, power, (power-gradient)}
+    std::string INTRACELL_TECHNIQUE = "naive";  // {naive, (margin), (edge)}
 
-    int N_mu = 20;
+    int N_mu = 20;                              // Number of angles to calculate at each height in PP-algorithm
 
 
 
     // Reading MVcases from json file
-    std::fstream file_MVcases("../data_input/MVcases.json");
+    std::fstream file_MVcases("/home/gijs-hogeboom/dev/lwproj/data_input/MVcases.json");
     if (!file_MVcases.is_open())
     {
         std::cerr << "Could not open MVcases.json!" << std::endl;
@@ -31,103 +35,66 @@ int main()
     nlohmann::json j;
     file_MVcases >> j;
 
-    std::vector<float> arr_z = j["z_" + CASE];
-    std::vector<float> arr_zh = j["zh_" + CASE];
-    std::vector<float> arr_dz = j["dz_" + CASE];
+    std::vector<float> arr_z = j["z_" + CASE].get<std::vector<float>>();
+    std::vector<float> arr_zh = j["zh_" + CASE].get<std::vector<float>>();
+    std::vector<float> arr_dz = j["dz_" + CASE].get<std::vector<float>>();
+
+    std::vector<float> arr_kext = j["kext_" + CASE].get<std::vector<float>>();
+    std::vector<float> arr_Batm = j["Batm_" + CASE].get<std::vector<float>>();
+    std::vector<float> arr_Batmh = j["Batmh_" + CASE].get<std::vector<float>>();
+
+    float Bsfc = j["Bsfc_" + CASE].get<float>();
 
 
-
-
-
-
-    // Initializing MC
+    // General initializations
     int Nphot = pow(2, Nphotpow);
     int Natm = Nphot / 2;
     int Nsfc = Nphot / 2;
 
+    int itot = arr_z.size();
+    int jtot = 1;
+    int ktot = 1;
+
+    float xtot = ktot * dx;
+    float ytot = jtot * dy;
+    float ztot = arr_zh[arr_zh.size() - 1];
 
 
-    std::vector<float> arr_x = {1,2,3,4,5};
-    std::vector<float> arr_y = {1,2,3,4,5};
-    // std::vector<float> arr_z = {1,2,3,4,5};
-    std::vector<float> arr_xh = {1,2,3,4,5};
-    std::vector<float> arr_yh = {1,2,3,4,5};
-    // std::vector<float> arr_zh = {1,2,3,4,5};
-    std::vector<float> field_kext = {1,2,3,4,5};
-    std::vector<float> arr_field_bounds= {1,2,3,4,5};
-    std::vector<float> arr_photons_atm_pos_x = {1,2,3,4,5};
-    std::vector<float> arr_photons_atm_pos_y = {1,2,3,4,5};
-    std::vector<float> arr_photons_atm_pos_z = {1,2,3,4,5};
-    std::vector<float> arr_photons_atm_mu = {1,2,3,4,5};
-    std::vector<float> arr_photons_atm_az = {1,2,3,4,5};
-    std::vector<float> arr_photons_atm_tau = {1,2,3,4,5};
-    std::vector<float> arr_power_per_photon_atm = {1,2,3,4,5};
-    std::vector<float> arr_photons_sfc_pos_x = {1,2,3,4,5};
-    std::vector<float> arr_photons_sfc_pos_y = {1,2,3,4,5};
-    std::vector<float> arr_photons_sfc_pos_z = {1,2,3,4,5};
-    std::vector<float> arr_photons_sfc_mu = {1,2,3,4,5};
-    std::vector<float> arr_photons_sfc_az = {1,2,3,4,5};
-    std::vector<float> arr_photons_sfc_tau = {1,2,3,4,5};
-    std::vector<float> arr_power_per_photon_sfc = {1,2,3,4,5};
 
-    std::vector<float> arr_kext = {1,2,3,4,5};
-    std::vector<float> arr_Batm = {1,2,3,4,5};
-    std::vector<float> arr_Batmh = {1,2,3,4,5};
+    if (ENABLE_MC)
+    {
+        std::vector<float> heating_rates_MC = run_MC(arr_z,
+                                                     arr_zh,
+                                                     arr_dz,
+                                                     Bsfc,
+                                                     dx,
+                                                     dy,
+                                                     ktot,
+                                                     jtot,
+                                                     itot,
+                                                     arr_kext,
+                                                     arr_Batm,
+                                                     CASE,
+                                                     INTERCELL_TECHNIQUE,
+                                                     INTRACELL_TECHNIQUE,
+                                                     Natm,
+                                                     Nsfc);
+    }
 
 
-    std::vector<float> heating_rates_PP = run_plane_parallel(arr_z,
-                                                            arr_zh,
-                                                            arr_dz,
-                                                            arr_kext,
-                                                            arr_Batm,
-                                                            arr_Batmh,
-                                                            CASE,
-                                                            N_mu);
+    if (ENABLE_PP)
+    {
+        std::vector<float> heating_rates_PP = run_plane_parallel(arr_z,
+                                                                arr_zh,
+                                                                arr_dz,
+                                                                arr_kext,
+                                                                arr_Batm,
+                                                                arr_Batmh,
+                                                                CASE,
+                                                                N_mu);
+    }
+
     
-    std::vector<float> heating_rates_MC = run_MC(arr_x,
-                                                 arr_y,
-                                                 arr_z,
-                                                 arr_xh,
-                                                 arr_yh,
-                                                 arr_zh,
-                                                 arr_dz,
-                                                 field_kext,
-                                                 arr_field_bounds,
-                                                 arr_photons_atm_pos_x,
-                                                 arr_photons_atm_pos_y,
-                                                 arr_photons_atm_pos_z,
-                                                 arr_photons_atm_mu,
-                                                 arr_photons_atm_az,
-                                                 arr_photons_atm_tau,
-                                                 arr_power_per_photon_atm,
-                                                 arr_photons_sfc_pos_x,
-                                                 arr_photons_sfc_pos_y,
-                                                 arr_photons_sfc_pos_z,
-                                                 arr_photons_sfc_mu,
-                                                 arr_photons_sfc_az,
-                                                 arr_photons_sfc_tau,
-                                                 arr_power_per_photon_sfc,
-                                                 CASE,
-                                                 INTERCELL_TECHNIQUE,
-                                                 INTRACELL_TECHNIQUE,
-                                                 Natm,
-                                                 Nsfc);
 
-
-
-    std::cout << "Heating rates:" << std::endl;
-    std::cout << "PP:" << std::endl;
-    for (auto el : heating_rates_PP)
-    {
-        std::cout << el << ',';
-    }
-    std::cout << std::endl;
-    std::cout << "MC:" << std::endl;
-
-    for (auto el : heating_rates_PP)
-    {
-        std::cout << el << ',';
-    }
-    std::cout << std::endl;
     return 0;
 }
